01adac7faabcd31fc961110229eb73c6d76d94f9
lbarjak <barjak.laszlo@gmail.com>
2021-01-21 17:21:20 +0100

    refactored

--- main.js
+++ main.js
@@ -8,26 +8,27 @@ function start() {
   }
   fifths.innerHTML += "-------";
   let step = Math.pow(2, 1 / 12);
-  let chromatic_fifth = Math.pow(step, 7);
-  let natural_fifth = 1.5;
+  let fifth_chromatic = Math.pow(step, 7);
+  let fifth_natural = 1.5;
   notes_chromatic = [];
   notes_natural = [];
-  notes_chromatic = put_fifths(start_note, chromatic_fifth, notes_chromatic);
-  notes_natural = put_fifths(start_note, natural_fifth, notes_natural);
+  notes_chromatic = put_fifths(fifth_chromatic, notes_chromatic);
+  notes_natural = put_fifths(fifth_natural, notes_natural);
   write_fifths(notes_chromatic);
   fifths.innerHTML += "-------";
   write_fifths(notes_natural);
 
-  function put_fifths(start_note, typeOfFifth, notes) {
+  function put_fifths(typeOfFifth, notes) {
     let actual_fifth;
+    let local_start_note = start_note;
     notes = [];
     for (let i = 0; i <= 12; i++) {
-      actual_fifth = start_note;
-      while (actual_fifth > 900) {
+      actual_fifth = local_start_note;
+      while (actual_fifth > start_note * 2.1) {
         actual_fifth = actual_fifth / 2;
       }
       notes.push(actual_fifth.toFixed(2));
-      start_note = start_note * typeOfFifth;
+      local_start_note = local_start_note * typeOfFifth;
     }
     notes.sort();
     return notes;
