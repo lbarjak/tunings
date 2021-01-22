087cc4d71f75e41368ebdd6ec8d9d4b888738082
lbarjak <barjak.laszlo@gmail.com>
2021-01-22 20:20:01 +0100

    oscillators arrays

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let fifths;
 let notes_chromatic = [];
 let notes_natural = [];
 let btns = [];
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let oscillators = [[],[]];
 
 function start() {
   fifths = document.getElementById("fifths");
@@ -57,11 +59,25 @@ function write_fifths() {
   }
   for (let i = 0; i <= 12; i++) {
     btns[i] = document.querySelector('input[name="button' + i + '"]');
+
+    oscillators[0][i] = audioCtx.createOscillator();
+    oscillators[0][i].type = 'sine';
+    oscillators[0][i].frequency.value = notes_chromatic[i];
+    oscillators[1][i] = audioCtx.createOscillator();
+    oscillators[1][i].type = 'sine';
+    oscillators[1][i].frequency.value = notes_natural[i];
+
     btns[i].onmousedown = function () {
       console.log("btns[" + i + "] down");
+      oscillators[0][i].start(0);
+      oscillators[0][i].connect(audioCtx.destination);
+      oscillators[1][i].start(0);
+      oscillators[1][i].connect(audioCtx.destination);
     };
     btns[i].onmouseup = function () {
       console.log("btns[" + i + "] up");
+      oscillators[0][i].stop(0);
+      oscillators[1][i].stop(0);
     };
   }
 }
