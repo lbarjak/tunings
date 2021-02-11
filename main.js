2ae8033a155c8c331ca5d9a389753399298af421
lbarjak <barjak.laszlo@gmail.com>
2021-02-11 13:34:10 +0100

    - main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import Midi from './midi.js'
-import View from './view.js'
-
-export default class Main {
-    constructor() {
-        this.midi = new Midi()
-        this.view = new View()
-    }
-}
