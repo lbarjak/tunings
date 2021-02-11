be1febd86a851d4580a47494097856cc5cfa3c4e
lbarjak <barjak.laszlo@gmail.com>
2021-02-11 12:38:33 +0100

    separately tunings.js

--- synthesizer.js
+++ synthesizer.js
@@ -1,6 +1,7 @@
 //based on https://github.com/uber5001/CanvasKeyboard
 
 import adsr from './adsr.js'
+import Tunings from './tunings.js'
 
 export default class Synthesizer {
     static instance = null
@@ -18,43 +19,14 @@ export default class Synthesizer {
             window.mozAudioContext ||
             window.oAudioContext ||
             window.msAudioContext)()
-        /*         this.gain = this.context.createGain()
-        this.gain.gain.value = 0.1
-        this.gain.connect(this.context.destination) */
-        //this.type = 'square'
         this.context.resume()
-        this.tuningEqual = new Array(129).fill(0)
-        this.tuningEqualF()
-        this.tuningCircleOfFifth = new Array(129).fill(0)
-        this.tuningCircleOfFifthF()
+        this.tunings = new Tunings()
+        this.tuningEqual = this.tunings.tuningEqual
+        this.tunings.tuningEqualF()
+        this.tuningCircleOfFifth = this.tunings.tuningCircleOfFifth
+        this.tunings.tuningCircleOfFifthF()
     }
-    tuningEqualF() {
-        //117 A8 7040.00 (7040 / 2^4 = 440)
-        for (let i = 117; i <= 128; i++) {
-            this.tuningEqual[i] = 440 * Math.pow(2, (i - 69) / 12)
-        }
-        for (let i = 128; i > 11; i--) {
-            this.tuningEqual[i - 12] = this.tuningEqual[i] / 2
-        }
-    }
-    tuningCircleOfFifthF() {
-        let startNote = 8343.703703703702
-        let localStartNote = startNote
-        let naturalFifth = 1.5
-        let actualFifth
-        for (let i = 120; i <= 131; i++) {
-            actualFifth = localStartNote
-            while (actualFifth > startNote * 2.1) {
-                actualFifth = actualFifth / 2
-            }
-            this.tuningCircleOfFifth[i] = actualFifth
-            localStartNote = localStartNote * naturalFifth
-        }
-        this.tuningCircleOfFifth.sort((a, b) => a - b)
-        for (let i = 131; i > 11; i--) {
-            this.tuningCircleOfFifth[i - 12] = this.tuningCircleOfFifth[i] / 2
-        }
-    }
+
     noteOn(note, channel) {
         if (channel > 1) {
             console.log('There is not a tunning on this channel!')
---
+++ tunings.js
@@ -0,0 +1,33 @@
+export default class Tunings {
+    constructor() {
+        this.tuningEqual = new Array(129).fill(0)
+        this.tuningCircleOfFifth = new Array(129).fill(0)
+    }
+    tuningEqualF() {
+        //117 A8 7040.00 (7040 / 2^4 = 440)
+        for (let i = 117; i <= 128; i++) {
+            this.tuningEqual[i] = 440 * Math.pow(2, (i - 69) / 12)
+        }
+        for (let i = 128; i > 11; i--) {
+            this.tuningEqual[i - 12] = this.tuningEqual[i] / 2
+        }
+    }
+    tuningCircleOfFifthF() {
+        let startNote = 8343.703703703702
+        let localStartNote = startNote
+        let naturalFifth = 1.5
+        let actualFifth
+        for (let i = 120; i <= 131; i++) {
+            actualFifth = localStartNote
+            while (actualFifth > startNote * 2.1) {
+                actualFifth = actualFifth / 2
+            }
+            this.tuningCircleOfFifth[i] = actualFifth
+            localStartNote = localStartNote * naturalFifth
+        }
+        this.tuningCircleOfFifth.sort((a, b) => a - b)
+        for (let i = 131; i > 11; i--) {
+            this.tuningCircleOfFifth[i - 12] = this.tuningCircleOfFifth[i] / 2
+        }
+    }
+}
